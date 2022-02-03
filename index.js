bec2732843a6dfec1fbcf4f3a0a72fad53e4d84d
Cat Brownie <catbrownie@Peachy.local>
2022-02-03 12:18:38 -0800

    Styling changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ fetch("https://api.magicthegathering.io/v1/cards")
 .then(res => res.json())
 .then(respos => cards = respos)
 .then(data => {
-console.log("number of cards", data.cards.length)
+console.log("number of cards", data.cards)
 let filteredArray = data.cards
 })
 
