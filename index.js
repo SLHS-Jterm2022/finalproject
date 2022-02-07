72ca7a34da22c01989f1ee128748fbedaebdac3e
Jonah Day <dayjm@school.hopeseattle.org>
2022-02-07 12:18:18 -0800

    monday changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ fetch("https://api.magicthegathering.io/v1/cards")
 .then(res => res.json())
 .then(respos => cards = respos)
 .then(data => {
-console.log("number of cards", data.cards.length)
+console.log("number of cards", data.cards)
 let filteredArray = data.cards.filter(ele => ele.imageUrl)
 for(const ele of filteredArray) {
    let mainEL = document.getElementById('main')
@@ -19,8 +19,9 @@ for(const ele of filteredArray) {
 })
 
 function cardClicked(event) {
-    console.log("Event", event.target)
-
+    console.log("Event", event.target.getAttribute('name'))
+    localStorage.setItem("cardNumber", event.target.getAttribute('name'))
+    
 
 
 }
@@ -61,3 +62,76 @@ console.log("First Value", firstValue)
 
 
 //made an alteration
+
+const menu1 = document.getElementById("menu1");
+const menu2 = document.getElementById("menu2");
+const menu3 = document.getElementById("menu3");
+const menu4 = document.getElementById("menu4");
+const menu5 = document.getElementById("menu5");
+const menu6 = document.getElementById("menu6");
+const menu7 = document.getElementById("menu7");
+
+menu1.addEventListener("change", function(event) {
+    console.log("Event: Menu 1 change", event.target.value)
+    let value = event.target.value;
+    
+    if(value === "0") {
+        menu2.setAttribute("class", "show")
+        menu3.setAttribute("class", "hide")
+        menu4.setAttribute("class", "hide")
+        menu5.setAttribute("class", "hide")
+        menu6.setAttribute("class", "hide")
+        menu7.setAttribute("class", "hide")
+        
+        menu2.addEventListener("change", function(event) {
+            console.log("If statement", event.target.value)
+            console.log('type of cards' , typeof(cards));
+            let filteredArray = cards.filter(card => card.colors === event.target.value)
+        })
+    }
+
+    if(value === "1") {
+        menu2.setAttribute("class", "hide")
+        menu3.setAttribute("class", "show")
+        menu4.setAttribute("class", "hide")
+        menu5.setAttribute("class", "hide")
+        menu6.setAttribute("class", "hide")
+        menu7.setAttribute("class", "hide")
+    }
+
+    if(value === "2") {
+        menu2.setAttribute("class", "hide")
+        menu3.setAttribute("class", "hide")
+        menu4.setAttribute("class", "show")
+        menu5.setAttribute("class", "hide")
+        menu6.setAttribute("class", "hide")
+        menu7.setAttribute("class", "hide")
+    }
+
+    if(value === "3") {
+        menu2.setAttribute("class", "hide")
+        menu3.setAttribute("class", "hide")
+        menu4.setAttribute("class", "hide")
+        menu5.setAttribute("class", "show")
+        menu6.setAttribute("class", "hide")
+        menu7.setAttribute("class", "hide")
+    }
+
+    if(value === "4") {
+        menu2.setAttribute("class", "hide")
+        menu3.setAttribute("class", "hide")
+        menu4.setAttribute("class", "hide")
+        menu5.setAttribute("class", "hide")
+        menu6.setAttribute("class", "show")
+        menu7.setAttribute("class", "hide")
+    }
+
+    if(value === "5") {
+        menu2.setAttribute("class", "hide")
+        menu3.setAttribute("class", "hide")
+        menu4.setAttribute("class", "hide")
+        menu5.setAttribute("class", "hide")
+        menu6.setAttribute("class", "hide")
+        menu7.setAttribute("class", "show")
+    }
+})
